2d86d61da91cea42a63e63156b50275196636cb1
Francis dela Cruz <franciszac.dlc@gmail.com>
2018-03-10 17:09:34 +0800

    Move front page, add post preview

---
+++ preview.php
@@ -0,0 +1,20 @@
+<div class="col-sm-6 post-preview-box">
+  <div class="post-preview">
+    <!-- Title -->
+    <div class="post-preview-title">
+      <h1><?php the_title(); ?></h1>
+    </div>
+
+    <!-- Author -->
+    <p class="lead">
+    by
+    <a href="#"><?php the_author(); ?></a>
+    on <?php the_date(); ?>
+    </p>
+
+    <!-- Preview Image -->
+    <!--img class="img-fluid rounded" src="http://placehold.it/900x300" alt="">
+
+    <hr-->
+  </div>
+</div>
